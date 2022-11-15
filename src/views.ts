11c7212f85a4e0cd3ed2be27791ae515b65cc9ad
Linxzh <linxzh1989@gmail.com>
2022-11-15 17:50:26 +0800

    ui: add menuitem on Zotero toolbar

--- src/views.ts
+++ src/views.ts
@@ -20,14 +20,42 @@ class AddonViews extends AddonModule {
     // You can init the UI elements that
     // cannot be initialized with overlay.xul
     console.log("Initializing UI");
-    const _window: Window = _Zotero.getMainWindow();
-    const button = _window.document.createElement("toolbarbutton");
-    button.id = "zotero-tb-tara";
-    button.setAttribute("type", "menu");
-    button.className = "zotero-tb-button";
-    button.style["list-style-image"] = 
+    var _window: Window = _Zotero.getMainWindow();
+    var tool_button = _window.document.createElement("toolbarbutton");
+    var menupopup = _window.document.createElement("menupopup");
+    var create_menu = _window.document.createElement("menuitem");
+    var export_menu = _window.document.createElement("menuitem");
+    var restore_menu = _window.document.createElement("menuitem");
+
+    create_menu.setAttribute("id", "zotero-tb-tara-create-backup");
+    create_menu.setAttribute("label", "Create");
+    create_menu.setAttribute("class", "menuitem-iconic");
+    create_menu.setAttribute("style", "list-style-image: url('chrome://tara/skin/create_icon.png');");
+    create_menu.setAttribute("oncommand", "alert('create');");
+
+    export_menu.setAttribute("id", "zotero-tb-tara-export-backup");
+    export_menu.setAttribute("label", "Export");
+    export_menu.setAttribute("class", "menuitem-iconic");
+    export_menu.setAttribute("style", "list-style-image: url('chrome://tara/skin/export_icon.png');");
+    export_menu.setAttribute("oncommand", "Zotero.Tara.utils.createBackupZIP();");
+
+    restore_menu.setAttribute("id", "zotero-tb-tara-restore-backup");
+    restore_menu.setAttribute("label", "Restore");
+    restore_menu.setAttribute("class", "menuitem-iconic");
+    restore_menu.setAttribute("style", "list-style-image: url('chrome://tara/skin/restore_icon.png');");
+    restore_menu.setAttribute("oncommand", "alert('Restore');");
+
+    menupopup.appendChild(create_menu);
+    menupopup.appendChild(export_menu);
+    menupopup.appendChild(restore_menu);
+
+    tool_button.id = "zotero-tb-tara";
+    tool_button.setAttribute("type", "menu");
+    tool_button.className = "zotero-tb-button";
+    tool_button.style["list-style-image"] = 
       "url('chrome://tara/skin/tara_icon.png')";
-    _window.document.querySelector("#zotero-collections-toolbar").appendChild(button);
+    tool_button.appendChild(menupopup);
+    _window.document.querySelector("#zotero-collections-toolbar").appendChild(tool_button);
   }
 
   public unInitViews(_Zotero) {
