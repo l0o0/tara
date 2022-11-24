36949ff0a3a8a32a2c1fd69dafe8ff14322c94e0
Linxzh <linxzh1989@gmail.com>
2022-11-24 17:39:53 +0800

    ui: update progress window when backup

--- src/utils.ts
+++ src/utils.ts
@@ -14,10 +14,10 @@ class Utils extends AddonModule {
         return OS.Path.join(profileDir, "prefs.js");
     }
 
-
     public async readPrefsFromFile() {
         let prefsFile: string = this.getPrefsPath();
-        let prefs: Map<string, any> = await this._Addon._Zotero.Profile.readPrefsFromFile(prefsFile);
+        let prefs: Map<string, any> =
+            await this._Addon._Zotero.Profile.readPrefsFromFile(prefsFile);
         return prefs;
     }
 
@@ -34,7 +34,7 @@ class Utils extends AddonModule {
             "extensions.zotero.sync.reminder.setUp.lastDisplayed",
             "extensions.zotero.sync.storage.verified",
             "extensions.zotero.recentSaveTargets",
-            "extensions.zotero.lastViewedFolder"  // Last viewd collection
+            "extensions.zotero.lastViewedFolder", // Last viewd collection
         ];
 
         for (let p in prefs) {
@@ -46,14 +46,10 @@ class Utils extends AddonModule {
     }
 
     public async getAddonInfos() {
-        let keepKeys: Array<string> = [
-            'id',
-            "userDisabled",
-            "path"
-        ];
+        let keepKeys: Array<string> = ["id", "userDisabled", "path"];
         let wordPlugins: Array<string> = [
-            'zoteroOpenOfficeIntegration@zotero.org',
-            'zoteroWinWordIntegration@zotero.org'
+            "zoteroOpenOfficeIntegration@zotero.org",
+            "zoteroWinWordIntegration@zotero.org",
         ];
 
         // Sometimes will throw Error: [JavaScript Error: "addon.getResourceURI is not a function"
@@ -65,21 +61,27 @@ class Utils extends AddonModule {
         //     addonManager = tmpWin.AddonManager;
         // }
         // let infos: Array<any> = await addonManager.getAllAddons();
-        let extensions = OS.Path.join(this._Addon._Zotero.Profile.dir, "extensions.json");
-        let extensionsContents = this._Addon._Zotero.File.getContents(this._Addon._Zotero.File.pathToFile(extensions));
+        let extensions = OS.Path.join(
+            this._Addon._Zotero.Profile.dir,
+            "extensions.json"
+        );
+        let extensionsContents = this._Addon._Zotero.File.getContents(
+            this._Addon._Zotero.File.pathToFile(extensions)
+        );
         let extensionsInfo = JSON.parse(extensionsContents);
-        
-        let infos = extensionsInfo['addons'];
 
-        infos = infos.filter(e => e.type == 'extension' &&
-            !(wordPlugins.includes(e.id))
+        let infos = extensionsInfo["addons"];
+
+        infos = infos.filter(
+            (e) => e.type == "extension" && !wordPlugins.includes(e.id)
         );
 
         let filteredInfos = infos.map(function (e) {
-            var out = {"name": e.defaultLocale.name};
-            out = keepKeys.reduce(
-                (p, c) => { p[c] = e[c]; return p },
-                out);
+            var out = { name: e.defaultLocale.name };
+            out = keepKeys.reduce((p, c) => {
+                p[c] = e[c];
+                return p;
+            }, out);
             return out;
         });
         return filteredInfos;
@@ -94,13 +96,13 @@ class Utils extends AddonModule {
      * @returns Array of translator info map.
      */
     public async getTranslatorInfos() {
-        var infos =  await this._Addon._Zotero.Translators.getAll();
+        var infos = await this._Addon._Zotero.Translators.getAll();
         let keepKeys = ["translatorID", "path", "fileName"];
         let filteredInfos = infos.map(function (e) {
-            return keepKeys.reduce(
-                (p, c) => {p[c] = e[c]; return p},
-                {}
-            )
+            return keepKeys.reduce((p, c) => {
+                p[c] = e[c];
+                return p;
+            }, {});
         });
         return filteredInfos;
     }
@@ -111,20 +113,21 @@ class Utils extends AddonModule {
         let cslInfos = this.getStyleInfos();
         let tInfos = await this.getTranslatorInfos();
         return {
-            "createTime": (new Date()).toISOString(),
-            "meta": {
-                "prefNum": Object.keys(prefsInfos).length, 
-                "addonNum": addonInfos.length, 
-                "cslNum": Object.keys(cslInfos).length, 
-                "tNum": tInfos.length},
-            "preferences": prefsInfos,
-            "addons" : addonInfos,
-            "styles": cslInfos,
-            "translators": tInfos
-        }
+            createTime: new Date().toISOString(),
+            meta: {
+                prefNum: Object.keys(prefsInfos).length,
+                addonNum: addonInfos.length,
+                cslNum: Object.keys(cslInfos).length,
+                tNum: tInfos.length,
+            },
+            preferences: prefsInfos,
+            addons: addonInfos,
+            styles: cslInfos,
+            translators: tInfos,
+        };
     }
 
-    public async createBackupZIP(isExport=false) {
+    public async createBackupZIP(isExport = false) {
         // Create a temporary folder. Data in backup folder
         let cacheTmp = this._Addon._Zotero.getTempDirectory();
         const tmpDir = cacheTmp.path;
@@ -139,43 +142,66 @@ class Utils extends AddonModule {
         }
 
         const outDir = OS.Path.join(tmpDir, "Backup");
-        await this._Addon._Zotero.File.createDirectoryIfMissingAsync(
-            outDir
-        );
+        await this._Addon._Zotero.File.createDirectoryIfMissingAsync(outDir);
+        var backupInfos;
+        try {
+            backupInfos = await this.getBackupInfos();
+            let backupInfosText = JSON.stringify(backupInfos);
+            // Save preference
+            let pf = OS.Path.join(outDir, "backup.json");
+            this._Addon._Zotero.File.putContents(
+                this._Addon._Zotero.File.pathToFile(pf),
+                backupInfosText
+            );
+            this._Addon.views.updateProgressWindow("preferences", true);
+        } catch (e) {
+            this._Addon.views.updateProgressWindow("preferences", false);
+        }
 
-        let backupInfos = await this.getBackupInfos();
-        let backupInfosText = JSON.stringify(backupInfos);
-        // Save preference
-        let pf = OS.Path.join(outDir, "backup.json");
-        this._Addon._Zotero.File.putContents(this._Addon._Zotero.File.pathToFile(pf), backupInfosText);
         // Copy folders.
         let s: string, t: string;
         let profileDir: string = this._Addon._Zotero.Profile.dir;
         let dataDir: string = this._Addon._Zotero.Prefs.get("dataDir");
-        if (backupInfos.meta.addonNum > 0) {
-            s = OS.Path.join(profileDir, 'extensions');
-            t = OS.Path.join(outDir, 'extensions');
-            await this._Addon._Zotero.File.copyDirectory(s, t);
+        try {
+            if (backupInfos.meta.addonNum > 0) {
+                s = OS.Path.join(profileDir, "extensions");
+                t = OS.Path.join(outDir, "extensions");
+                await this._Addon._Zotero.File.copyDirectory(s, t);
+                this._Addon.views.updateProgressWindow("addons", true);
+            }
+        } catch (e) {
+            this._Addon.views.updateProgressWindow("addons", false);
         }
-        if (backupInfos.meta.cslNum > 0) {
-            s = OS.Path.join(dataDir, 'styles');
-            t = OS.Path.join(outDir, 'styles');
-            await this._Addon._Zotero.File.copyDirectory(s, t);
+        try {
+            if (backupInfos.meta.cslNum > 0) {
+                s = OS.Path.join(dataDir, "styles");
+                t = OS.Path.join(outDir, "styles");
+                await this._Addon._Zotero.File.copyDirectory(s, t);
+                this._Addon.views.updateProgressWindow("styles", true);
+            }
+        } catch (e) {
+            this._Addon.views.updateProgressWindow("styles", false);
         }
-        if (backupInfos.meta.tNum > 0) {
-            s = OS.Path.join(dataDir, 'translators');
-            t = OS.Path.join(outDir, 'translators');
-            await this._Addon._Zotero.File.copyDirectory(s, t);
+        try {
+            if (backupInfos.meta.tNum > 0) {
+                s = OS.Path.join(dataDir, "translators");
+                t = OS.Path.join(outDir, "translators");
+                await this._Addon._Zotero.File.copyDirectory(s, t);
+                this._Addon.views.updateProgressWindow("translators", true);
+            }
+        } catch (e) {
+            this._Addon.views.updateProgressWindow("translators", false);
         }
         // Locate engine.json file
-        s = OS.Path.join(dataDir, 'locate');
-        t = OS.Path.join(outDir, 'locate');
+        s = OS.Path.join(dataDir, "locate");
+        t = OS.Path.join(outDir, "locate");
         await this._Addon._Zotero.File.copyDirectory(s, t);
 
         if (isExport) {
             await this._Addon._Zotero.File.copyToUnique(
                 OS.Path.join(profileDir, "extensions", "tara.xpi"),
-                OS.Path.join(outDir, "tara.xpi"));
+                OS.Path.join(outDir, "tara.xpi")
+            );
         }
         let saveDir = isExport ? OS.Path.join(dataDir, "Backup") : tmpDir;
         await this._Addon._Zotero.File.zipDirectory(
@@ -187,6 +213,7 @@ class Utils extends AddonModule {
 
     public async createBackupAsAttachment() {
         this._Addon._Zotero.debug("** Tara start create Backup As Attachment");
+        await this._Addon.views.openProgressWindow();
         await this.createBackupZIP();
         if (this._Addon._Zotero.Prefs.get("tara.itemid") == undefined) {
             let item = new this._Addon._Zotero.Item("document");
@@ -194,20 +221,23 @@ class Utils extends AddonModule {
             let itemID = await item.saveTx();
             this._Addon._Zotero.Prefs.set("tara.itemid", itemID);
         }
-        const zipfile: string = OS.Path.join(this._Addon._Zotero.Prefs.get("dataDir"),
-            "tmp", "backup.zip");
-        const item = this._Addon._Zotero.Items.get(this._Addon._Zotero.Prefs.get("tara.itemid"));
+        const zipfile: string = OS.Path.join(
+            this._Addon._Zotero.Prefs.get("dataDir"),
+            "tmp",
+            "backup.zip"
+        );
+        const item = this._Addon._Zotero.Items.get(
+            this._Addon._Zotero.Prefs.get("tara.itemid")
+        );
         const timeString = new Date().toISOString();
         const importOptions = {
-            file : zipfile,
+            file: zipfile,
             title: timeString + "_backup.zip",
-            parentItemID : item.id,
+            parentItemID: item.id,
         };
         await this._Addon._Zotero.Attachments.importFromFile(importOptions);
         this._Addon._Zotero.debug("** Tara finish create Backup As Attachment");
     }
-
-
 }
 
 export default Utils;
--- src/views.ts
+++ src/views.ts
@@ -3,94 +3,144 @@ import AddonModule from "./module";
 const { addonRef } = require("../package.json");
 
 class AddonViews extends AddonModule {
-  // You can store some element in the object attributes
-  private tickIcon: string;
-  private crossIcon: string;
-  private processWindow: any;
+    // You can store some element in the object attributes
+    private tickIcon: string;
+    private crossIcon: string;
+    private progressWindow: any;
 
-  constructor(parent: Addon) {
-    super(parent);
-      this.tickIcon = "chrome://zotero/skin/tick.png";
-      this.crossIcon =  "chrome://zotero/skin/cross.png";
-  }
-
-  public initViews(_Zotero) {
-    // You can init the UI elements that
-    // cannot be initialized with overlay.xul
-    console.log("Initializing UI");
-    var _window: Window = _Zotero.getMainWindow();
-    var tool_button = _window.document.createElement("toolbarbutton");
-    var menupopup = _window.document.createElement("menupopup");
-    var create_menu = _window.document.createElement("menuitem");
-    var export_menu = _window.document.createElement("menuitem");
-    var restore_menu = _window.document.createElement("menuitem");
-
-    const createLabel = this._Addon.locale.getString("toolbar.create");
-    const exportLabel = this._Addon.locale.getString("toolbar.export");
-    const restoreLabel = this._Addon.locale.getString("toolbar.restore");
-
-    create_menu.setAttribute("id", "zotero-tb-tara-create-backup");
-    create_menu.setAttribute("label", createLabel);
-    create_menu.setAttribute("class", "menuitem-iconic");
-    create_menu.setAttribute("style", "list-style-image: url('chrome://tara/skin/create_icon.png');");
-    create_menu.setAttribute("oncommand", "Zotero.Tara.utils.createBackupAsAttachment();");
-
-    export_menu.setAttribute("id", "zotero-tb-tara-export-backup");
-    export_menu.setAttribute("label", exportLabel);
-    export_menu.setAttribute("class", "menuitem-iconic");
-    export_menu.setAttribute("style", "list-style-image: url('chrome://tara/skin/export_icon.png');");
-    export_menu.setAttribute("oncommand", "Zotero.Tara.utils.createBackupZIP();");
-
-    restore_menu.setAttribute("id", "zotero-tb-tara-restore-backup");
-    restore_menu.setAttribute("label", restoreLabel);
-    restore_menu.setAttribute("class", "menuitem-iconic");
-    restore_menu.setAttribute("style", "list-style-image: url('chrome://tara/skin/restore_icon.png');");
-    restore_menu.setAttribute("oncommand", "alert('Restore');");
-
-    menupopup.appendChild(create_menu);
-    menupopup.appendChild(export_menu);
-    menupopup.appendChild(restore_menu);
-
-    tool_button.id = "zotero-tb-tara";
-    tool_button.setAttribute("type", "menu");
-    tool_button.className = "zotero-tb-button";
-    tool_button.style["list-style-image"] = 
-      "url('chrome://tara/skin/tara_icon.png')";
-    tool_button.appendChild(menupopup);
-    _window.document.querySelector("#zotero-collections-toolbar").appendChild(tool_button);
-  }
-
-  // Remove UI element when remove addon
-  public unInitViews(_Zotero) {
-    console.log("Uninitializing UI");
-    const _window: Window = _Zotero.getMainWindow();
-    _window.document
-      .querySelector("#zotero-tb-tara")
-      ?.remove();
-  }
-
-
-  public openProgressWindow(): void {
-    let win = Services.wm.getMostRecentWindow("navigator:browser");
-    if (win) {
-        this.processWindow = win.openDialog(
-            "chrome://tara/content/progress.html",
-            "",
-            "chrome,close=yes,resizable=yes,dependent,dialog,centerscreen");
+    constructor(parent: Addon) {
+        super(parent);
+        this.tickIcon = "chrome://zotero/skin/tick.png";
+        this.crossIcon = "chrome://zotero/skin/cross.png";
     }
-    else {
-        this.processWindow = Services.ww.openWindow(null, 
-            "chrome://tara/content/progress.html",
-            "", 
-            "chrome,close=yes,resizable=yes,dependent,dialog,centerscreen");
+
+    public initViews(_Zotero) {
+        // You can init the UI elements that
+        // cannot be initialized with overlay.xul
+        console.log("Initializing UI");
+        var _window: Window = _Zotero.getMainWindow();
+        var tool_button = _window.document.createElement("toolbarbutton");
+        var menupopup = _window.document.createElement("menupopup");
+        var create_menu = _window.document.createElement("menuitem");
+        var export_menu = _window.document.createElement("menuitem");
+        var restore_menu = _window.document.createElement("menuitem");
+
+        const createLabel = this._Addon.locale.getString("toolbar.create");
+        const exportLabel = this._Addon.locale.getString("toolbar.export");
+        const restoreLabel = this._Addon.locale.getString("toolbar.restore");
+
+        create_menu.setAttribute("id", "zotero-tb-tara-create-backup");
+        create_menu.setAttribute("label", createLabel);
+        create_menu.setAttribute("class", "menuitem-iconic");
+        create_menu.setAttribute(
+            "style",
+            "list-style-image: url('chrome://tara/skin/create_icon.png');"
+        );
+        create_menu.setAttribute(
+            "oncommand",
+            "Zotero.Tara.utils.createBackupAsAttachment();"
+        );
+
+        export_menu.setAttribute("id", "zotero-tb-tara-export-backup");
+        export_menu.setAttribute("label", exportLabel);
+        export_menu.setAttribute("class", "menuitem-iconic");
+        export_menu.setAttribute(
+            "style",
+            "list-style-image: url('chrome://tara/skin/export_icon.png');"
+        );
+        export_menu.setAttribute(
+            "oncommand",
+            "Zotero.Tara.utils.createBackupZIP();"
+        );
+
+        restore_menu.setAttribute("id", "zotero-tb-tara-restore-backup");
+        restore_menu.setAttribute("label", restoreLabel);
+        restore_menu.setAttribute("class", "menuitem-iconic");
+        restore_menu.setAttribute(
+            "style",
+            "list-style-image: url('chrome://tara/skin/restore_icon.png');"
+        );
+        restore_menu.setAttribute("oncommand", "alert('Restore');");
+
+        menupopup.appendChild(create_menu);
+        menupopup.appendChild(export_menu);
+        menupopup.appendChild(restore_menu);
+
+        tool_button.id = "zotero-tb-tara";
+        tool_button.setAttribute("type", "menu");
+        tool_button.className = "zotero-tb-button";
+        tool_button.style["list-style-image"] =
+            "url('chrome://tara/skin/tara_icon.png')";
+        tool_button.appendChild(menupopup);
+        _window.document
+            .querySelector("#zotero-collections-toolbar")
+            .appendChild(tool_button);
     }
-  }
 
-  public updateProgressWindow(row: string, status: string): void {
-    let doc = this.processWindow.document;
-    
-  }
+    // Remove UI element when remove addon
+    public unInitViews(_Zotero) {
+        console.log("Uninitializing UI");
+        const _window: Window = _Zotero.getMainWindow();
+        _window.document.querySelector("#zotero-tb-tara")?.remove();
+    }
 
+    public async openProgressWindow() {
+        this._Addon._Zotero.debug("** Tara open window ");
+        let win = Services.wm.getMostRecentWindow("navigator:browser");
+        if (win) {
+            this.progressWindow = win.openDialog(
+                "chrome://tara/content/progress.html",
+                "",
+                "chrome,close=yes,resizable=yes,dependent,dialog,centerscreen"
+            );
+        } else {
+            this.progressWindow = Services.ww.openWindow(
+                null,
+                "chrome://tara/content/progress.html",
+                "",
+                "chrome,close=yes,resizable=yes,dependent,dialog,centerscreen"
+            );
+        }
+        let t = 0;
+        // Wait for window
+        while (
+            t < 500 &&
+            this.progressWindow.document.readyState !== "complete"
+        ) {
+            await this._Addon._Zotero.Promise.delay(10);
+            t += 1;
+            this._Addon._Zotero.debug("** Tara wait ");
+        }
+    }
+
+    public closeProgressWindow(): void {
+        if (!this.progressWindow) {
+            return;
+        }
+        this.progressWindow.close();
+    }
+
+    public updateProgressWindow(row: string, status: boolean): void {
+        let doc = this.progressWindow.document;
+        var ele = doc.createElement("li");
+        ele.setAttribute("id", row);
+        var innerHTML: string;
+        if (status) {
+            innerHTML = `<img src="${
+                this.tickIcon
+            }"> Zotero ${this._Addon.locale.getString(
+                row
+            )} ${this._Addon.locale.getString("progress.backupsuccess")}`;
+        } else {
+            innerHTML = `<img src="${
+                this.crossIcon
+            }"> Zotero ${this._Addon.locale.getString(
+                row
+            )} ${this._Addon.locale.getString("progress.backupfail")}`;
+        }
+        ele.innerHTML = innerHTML;
+        doc.querySelector("#listbox").appendChild(ele);
+    }
 }
 
 export default AddonViews;
