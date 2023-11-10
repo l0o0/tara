566209e4c0550cc6eca16d4715b7f0c8549d2ea5
Linxzh <linxzh1989@gmail.com>
2023-11-10 16:25:50 +0800

    test: update code snippet

--- test/snipcode.js
+++ test/snipcode.js
@@ -1,7 +1,10 @@
 adm = ChromeUtils.import(
     "resource://gre/modules/AddonManager.jsm",
 );
-xpiFile = Zotero.File.pathToFile("C:\\Users\\E480\\AppData\\Local\\Temp\\Zotero\\Backup\\extensions\\Knowledge4Zotero@windingwind.com.xpi");
+xpiFile = Zotero.File.pathToFile("C:\\Users\\E480\\Downloads\\Knowledge4Zotero@windingwind.com.xpi");
+xpiFile = Zotero.File.pathToFile("C:\\Users\\E480\\Downloads\\pdfpreview@windingwind.com.xpi");
 
 installedResult =
     await adm.AddonManager.getInstallForFile(xpiFile);
+installedResult.addon.userDisabled = true;
+await installedResult.install();
