9b2197bda8d4dccfc7e1c5ce1509b687660f6696
l0o0 <linxzh1989@gmail.com>
2025-08-11 10:56:32 +0800

    fix: Progress window list display issue

--- src/modules/ui.ts
+++ src/modules/ui.ts
@@ -1,6 +1,5 @@
 import { config } from "../../package.json";
 import { getString } from "../utils/locale";
-import { getPref } from "../utils/prefs";
 import {
   createBackupAsAttachment,
   exportBackup,
@@ -123,8 +122,12 @@ export default class Progress {
   private crossIcon: string;
 
   constructor() {
-    this.tickIcon = "chrome://zotero/skin/tick.png";
-    this.crossIcon = "chrome://zotero/skin/cross.png";
+    this.tickIcon = `<svg class="icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+		<path fill="#4CAF50" d="m10.6 13.8l-2.15-2.15q-.275-.275-.7-.275t-.7.275t-.275.7t.275.7L9.9 15.9q.3.3.7.3t.7-.3l5.65-5.65q.275-.275.275-.7t-.275-.7t-.7-.275t-.7.275zM12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8" />
+</svg>`;
+    this.crossIcon = `<svg class="icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+		<path fill="#F44336" d="M12 17q.425 0 .713-.288T13 16t-.288-.712T12 15t-.712.288T11 16t.288.713T12 17m0-4q.425 0 .713-.288T13 12V8q0-.425-.288-.712T12 7t-.712.288T11 8v4q0 .425.288.713T12 13m0 9q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8" />
+	</svg>`;
   }
 
   getProgress(progress: number, total: number): string {
@@ -175,14 +178,14 @@ export default class Progress {
     ele.setAttribute("id", row);
     let innerHTML: string;
     if (status) {
-      innerHTML = `<img src="${this.tickIcon}"> ${row}`;
+      innerHTML = `${this.tickIcon} ${row}`;
       const value = `${(1 - this.queue!.length / this.totalTasks!) * 100}`;
       ztoolkit.log(
         `Progress ${value}, ${this.queue!.length} / ${this.totalTasks!}`,
       );
       doc.querySelector("#progress")!.setAttribute("value", value);
     } else {
-      innerHTML = `<img src="${this.crossIcon}"> ${row}`;
+      innerHTML = `${this.crossIcon} ${row}`;
     }
     ele.innerHTML = innerHTML;
     doc.querySelector("#listbox")!.appendChild(ele);
